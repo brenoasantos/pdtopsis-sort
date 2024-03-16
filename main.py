b1667c18f19722fc0cf376f2ec3afb7aa4caf944
guilhermercc <guilhermerccarvalho@gmail.com>
2024-03-15 22:34:22 -0300

    alter main

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from functions import PDTOPSIS_Sort
 
+# essa main será usada quando formos testar a interface
+'''
 def main():
     pdtopsis = PDTOPSIS_Sort(matrix_values_filepath="app_input/matrixValues.csv", input_filepath="app_input/input.csv")
+    pdtopsis.run_algorithm()
+'''
+def main():
+    pdtopsis = PDTOPSIS_Sort(matrix_values_filepath="matrixValues.csv", input_filepath="input.csv")
     pdtopsis.run_algorithm()
 
 if __name__ == "__main__":
