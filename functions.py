baa3779c25e2748f1405ad55557a03c1f83beb92
guilhermercc <guilhermerccarvalho@gmail.com>
2024-03-17 21:08:42 -0300

    add try, except and return to functions

--- functions.py
+++ functions.py
@@ -135,110 +135,150 @@ class PDTOPSIS_Sort:
             print(f"An error occurred: {e}")
 
     def calculate_complete_decision_matrix(self, decision_matrix, boundary_profiles, domain):
-        '''
-        Step 6.1: Create the Complete Decision Matrix by concatenating the decision matrix,
-        the boundary profiles, and the domain.
-        '''
-        self.complete_decision_matrix = np.vstack((decision_matrix, boundary_profiles, domain))
+        try:
+            '''
+            Step 6.1: Create the Complete Decision Matrix by concatenating the decision matrix,
+            the boundary profiles, and the domain.
+            '''
+            self.complete_decision_matrix = np.vstack((decision_matrix, boundary_profiles, domain))
+            
+            return self.complete_decision_matrix
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def normalize_decision_matrix(self):
-        '''
-        Step 6.2: Normalize the Complete Decision Matrix.
-        '''
-        self.normalized_decision_matrix = self.complete_decision_matrix / self.complete_decision_matrix.max(axis=0)
+        try:
+            '''
+            Step 6.2: Normalize the Complete Decision Matrix.
+            '''
+            self.normalized_decision_matrix = self.complete_decision_matrix / self.complete_decision_matrix.max(axis=0)
+            
+            return self.normalize_decision_matrix
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def calculate_weighted_normalized_decision_matrix(self, weights):
-        '''
-        Step 6.3: Calculate the weighted and normalized Decision Matrix.
-        '''
-        self.weighted_normalized_decision_matrix = self.normalized_decision_matrix * weights
+        try:
+            '''
+            Step 6.3: Calculate the weighted and normalized Decision Matrix.
+            '''
+            self.weighted_normalized_decision_matrix = self.normalized_decision_matrix * weights
+           
+            return self.weighted_normalized_decision_matrix
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def determine_ideal_and_anti_ideal_solutions(self, beneficial_criteria, cost_criteria):
-        '''
-        Step 6.4: Determine the ideal and anti-ideal solutions.
-        '''
-        self.v_star = np.max(self.weighted_normalized_decision_matrix[:, beneficial_criteria], axis=0)
-        self.v_minus = np.min(self.weighted_normalized_decision_matrix[:, beneficial_criteria], axis=0)
+        try:
+            '''
+            Step 6.4: Determine the ideal and anti-ideal solutions.
+            '''
+            self.v_star = np.max(self.weighted_normalized_decision_matrix[:, beneficial_criteria], axis=0)
+            self.v_minus = np.min(self.weighted_normalized_decision_matrix[:, beneficial_criteria], axis=0)
+            
+            return self.v_star, self.v_minus
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
 
     def calculate_distances(self):
-        '''
-        Step 6.5: Calculate the Euclidean distances from each alternative and profile to the ideal and anti-ideal solutions.
-        '''
-        # Inicializar as listas para armazenar as distâncias
-        self.distances_to_ideal = []
-        self.distances_to_anti_ideal = []
-        self.distances_to_ideal_profiles = []
-        self.distances_to_anti_ideal_profiles = []
+        try:
+            '''
+            Step 6.5: Calculate the Euclidean distances from each alternative and profile to the ideal and anti-ideal solutions.
+            '''
+            # Inicializar as listas para armazenar as distâncias
+            self.distances_to_ideal = []
+            self.distances_to_anti_ideal = []
+            self.distances_to_ideal_profiles = []
+            self.distances_to_anti_ideal_profiles = []
 
-        # Calcular as distâncias Euclidianas para cada alternativa (a_i)
-        for i in range(self.m):  # m é o número de alternativas
-            distance_to_ideal = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[i, :] - self.v_star) ** 2))
-            distance_to_anti_ideal = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[i, :] - self.v_minus) ** 2))
-            self.distances_to_ideal.append(distance_to_ideal)
-            self.distances_to_anti_ideal.append(distance_to_anti_ideal)
+            # Calcular as distâncias Euclidianas para cada alternativa (a_i)
+            for i in range(self.m):  # m é o número de alternativas
+                distance_to_ideal = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[i, :] - self.v_star) ** 2))
+                distance_to_anti_ideal = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[i, :] - self.v_minus) ** 2))
+                self.distances_to_ideal.append(distance_to_ideal)
+                self.distances_to_anti_ideal.append(distance_to_anti_ideal)
 
-        # Calcular as distâncias Euclidianas para cada perfil (P_k)
-        for k in range(self.q - 1):  # q é o número de perfis + 1
-            profile_index = k + self.m  # Perfis são indexados após as alternativas na matriz completa
-            distance_to_ideal_profile = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[profile_index, :] - self.v_star) ** 2))
-            distance_to_anti_ideal_profile = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[profile_index, :] - self.v_minus) ** 2))
-            self.distances_to_ideal_profiles.append(distance_to_ideal_profile)
-            self.distances_to_anti_ideal_profiles.append(distance_to_anti_ideal_profile)
+            # Calcular as distâncias Euclidianas para cada perfil (P_k)
+            for k in range(self.q - 1):  # q é o número de perfis + 1
+                profile_index = k + self.m  # Perfis são indexados após as alternativas na matriz completa
+                distance_to_ideal_profile = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[profile_index, :] - self.v_star) ** 2))
+                distance_to_anti_ideal_profile = np.sqrt(np.sum((self.weighted_normalized_decision_matrix[profile_index, :] - self.v_minus) ** 2))
+                self.distances_to_ideal_profiles.append(distance_to_ideal_profile)
+                self.distances_to_anti_ideal_profiles.append(distance_to_anti_ideal_profile)
+
+            return distances_to_ideal, distances_to_anti_ideal, distances_to_ideal_profiles, distances_to_anti_ideal_profiles
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def calculate_closeness_coefficients(self):
-        '''
-        Step 6.6: Calculate the closeness coefficients of each alternative and profile.
-        '''
-        # Inicializar as listas para armazenar os coeficientes de proximidade
-        self.closeness_coefficients = []
-        self.profiles_closeness_coefficients = []
+        try:
+            '''
+            Step 6.6: Calculate the closeness coefficients of each alternative and profile.
+            '''
+            # Inicializar as listas para armazenar os coeficientes de proximidade
+            self.closeness_coefficients = []
+            self.profiles_closeness_coefficients = []
 
-        # Calcular os coeficientes de proximidade para cada alternativa
-        for i in range(self.m):  # m é o número de alternativas
-            closeness_coefficient = self.distances_to_anti_ideal[i] / (self.distances_to_ideal[i] + self.distances_to_anti_ideal[i])
-            self.closeness_coefficients.append(closeness_coefficient)
+            # Calcular os coeficientes de proximidade para cada alternativa
+            for i in range(self.m):  # m é o número de alternativas
+                closeness_coefficient = self.distances_to_anti_ideal[i] / (self.distances_to_ideal[i] + self.distances_to_anti_ideal[i])
+                self.closeness_coefficients.append(closeness_coefficient)
 
-        # Calcular os coeficientes de proximidade para cada perfil
-        for k in range(self.q - 1):  # q é o número de perfis + 1
-            profile_closeness_coefficient = self.distances_to_anti_ideal_profiles[k] / (self.distances_to_ideal_profiles[k] + self.distances_to_anti_ideal_profiles[k])
-            self.profiles_closeness_coefficients.append(profile_closeness_coefficient)
+            # Calcular os coeficientes de proximidade para cada perfil
+            for k in range(self.q - 1):  # q é o número de perfis + 1
+                profile_closeness_coefficient = self.distances_to_anti_ideal_profiles[k] / (self.distances_to_ideal_profiles[k] + self.distances_to_anti_ideal_profiles[k])
+                self.profiles_closeness_coefficients.append(profile_closeness_coefficient)
 
+            return closeness_coefficients, profiles_closeness_coefficients
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def classify_alternatives(self, profiles_closeness_coefficients):
-        '''
-        Step 6.7: Classify the alternatives by making comparisons between their closeness coefficients.
-        '''
-        # Supondo que self.profiles_closeness_coefficients seja uma lista ordenada dos coeficientes de proximidade dos perfis
-        # Supondo também que self.closeness_coefficients contém os coeficientes de proximidade das alternativas
-        self.classifications = []  # Esta lista irá armazenar a classificação de cada alternativa
+        try:
+            '''
+            Step 6.7: Classify the alternatives by making comparisons between their closeness coefficients.
+            '''
+            # Supondo que self.profiles_closeness_coefficients seja uma lista ordenada dos coeficientes de proximidade dos perfis
+            # Supondo também que self.closeness_coefficients contém os coeficientes de proximidade das alternativas
+            self.classifications = []  # Esta lista irá armazenar a classificação de cada alternativa
 
-        # Iniciar a classificação
-        for closeness_coefficient in self.closeness_coefficients:
-            # Inicialmente, supõe-se que a alternativa não está classificada em nenhuma classe
-            class_assignment = None
+            # Iniciar a classificação
+            for closeness_coefficient in self.closeness_coefficients:
+                # Inicialmente, supõe-se que a alternativa não está classificada em nenhuma classe
+                class_assignment = None
 
-            # Verificar se a alternativa pertence à melhor classe C1
-            if closeness_coefficient >= self.profiles_closeness_coefficients[0]:
-                class_assignment = 'C1'
-            # Verificar se a alternativa pertence à pior classe Cq
-            elif closeness_coefficient < self.profiles_closeness_coefficients[-1]:
-                class_assignment = 'Cq'
-            # Se não, verificar as classes intermediárias
-            else:
-                for k in range(1, len(self.profiles_closeness_coefficients)):
-                    if self.profiles_closeness_coefficients[k-1] > closeness_coefficient >= self.profiles_closeness_coefficients[k]:
-                        class_assignment = f'C{k+1}'
-                        break
+                # Verificar se a alternativa pertence à melhor classe C1
+                if closeness_coefficient >= self.profiles_closeness_coefficients[0]:
+                    class_assignment = 'C1'
+                # Verificar se a alternativa pertence à pior classe Cq
+                elif closeness_coefficient < self.profiles_closeness_coefficients[-1]:
+                    class_assignment = 'Cq'
+                # Se não, verificar as classes intermediárias
+                else:
+                    for k in range(1, len(self.profiles_closeness_coefficients)):
+                        if self.profiles_closeness_coefficients[k-1] > closeness_coefficient >= self.profiles_closeness_coefficients[k]:
+                            class_assignment = f'C{k+1}'
+                            break
 
-            # Se, após as verificações, ainda não tiver sido possível classificar a alternativa
-            # isso indicaria um erro na lógica ou nos dados
-            if class_assignment is None:
-                raise ValueError('Não foi possível classificar a alternativa com base nos coeficientes de proximidade fornecidos.')
+                # Se, após as verificações, ainda não tiver sido possível classificar a alternativa
+                # isso indicaria um erro na lógica ou nos dados
+                if class_assignment is None:
+                    raise ValueError('Não foi possível classificar a alternativa com base nos coeficientes de proximidade fornecidos.')
 
-            self.classifications.append(class_assignment)
+                self.classifications.append(class_assignment)
 
-        # Retornar as classificações ou manipulá-las conforme necessário
-        return self.classifications
+            # Retornar as classificações
+            return self.classifications
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def sensitivity_analysis(self):
         # placeholder para análise de sensibilidade
